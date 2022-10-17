eb3c302172eb0063f93e8fc36497ea89edd97037
sahde <sahde@Akshay-laptop>
2022-10-17 14:42:36 +0530

    trying

---
+++ day8-DoubleLoop/src/fourth.java
@@ -0,0 +1,17 @@
+
+public class fourth {
+
+	public static void main(String[] args) {
+		
+		int count=0;
+		for(int i=1;i<=6;i++){
+			
+			for(int j=1;j<=i;j++){
+				
+				count=count+1;
+				System.out.print(count+" ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ day8-DoubleLoop/src/sesondpattern.java
@@ -0,0 +1,15 @@
+
+public class sesondpattern {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=6;i++){
+			
+			for(int j=6;j>=i;j--){
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+	}
+}
